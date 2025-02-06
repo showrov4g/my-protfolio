7633d263e56574331973595acb0e7e321b35dee8
showrov4g <showrovg00@gmail.com>
2025-02-06 21:00:00 +0600

    hero section button design

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,9 +5,9 @@ const Hero = () => {
         <div className="flex flex-col items-center gap-9">
             <img src={profileImg} className="w-60 rounded-full mt-36" alt="" />
             <h1 className="text-center  text-7xl font-semibold"><span className="hero-text">I'm Showrov Ghosh,</span> frontend developer based in Bangladesh</h1>
-            <p className="w-1/2 items-center text-center">I am a frontend developer from California, USA with 10 years of experience in multiple companies like Microsoft, Tesla and Apple.</p>
+            <p className="w-1/2 text-center text-2xl leading-10">I am a frontend developer from California, USA with 10 years of experience in multiple companies like Microsoft, Tesla and Apple.</p>
             {/* buttons  */}
-            <div>
+            <div className="flex items-center gap-6 text-2xl font-medium mb-12">
                 <div>Connect with me</div>
                 <div>My Resume</div>
             </div>
