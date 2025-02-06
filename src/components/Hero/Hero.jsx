0dbda0ca2b008ad5145ae7ac90b257541999be3a
showrov4g <showrovg00@gmail.com>
2025-02-06 20:37:15 +0600

    hero section paragraph tag design

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
         <div className="flex flex-col items-center gap-9">
             <img src={profileImg} className="w-60 rounded-full mt-36" alt="" />
             <h1 className="text-center  text-7xl font-semibold"><span className="hero-text">I'm Showrov Ghosh,</span> frontend developer based in Bangladesh</h1>
-            <p>I am a frontend developer from California, USA with 10 years of experience in multiple companies like Microsoft, Tesla and Apple.</p>
+            <p className="w-1/2 items-center text-center">I am a frontend developer from California, USA with 10 years of experience in multiple companies like Microsoft, Tesla and Apple.</p>
             {/* buttons  */}
             <div>
                 <div>Connect with me</div>
