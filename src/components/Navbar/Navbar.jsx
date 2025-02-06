4cc3a4d3b5e38f03417cf0063e523b37f2ba50f1
showrov4g <showrovg00@gmail.com>
2025-02-06 11:32:03 +0600

    navbar making

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,19 @@
-
+import logo from "../../assets/logo.svg"
 const Navbar = () => {
     return (
-        <div>
-            <h1 className="text-3xl font-bold">this is navbar</h1>
+        <div className="w-11/12 flex items-center justify-between my-1.5 mx-auto ">
+            <img src={logo} alt="" />
+            <ul>
+                <li>Home</li>
+                <li>About me</li>
+                <li>Services</li>
+                <li>Portfolio</li>
+                <li>Contact</li>
+            </ul>
+            {/* contact me button  */}
+            <div>
+                    <button>Connect with me</button>
+            </div>
         </div>
     );
 };
