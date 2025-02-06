7f80f060b12ebecf9950e50b1bbe8d22523ccadf
showrov4g <showrovg00@gmail.com>
2025-02-06 14:48:51 +0600

    hero text design

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,10 @@
 import profileImg from "../../assets/profile_img.png"
+import "../Hero/Hero.css"
 const Hero = () => {
     return (
         <div className="flex flex-col items-center gap-9">
-            <img src={profileImg} className="w-60 rounded-full" alt="" />
-            <h1>I'm Showrov Ghosh, frontend developer based in Bangladesh</h1>
+            <img src={profileImg} className="w-60 rounded-full mt-36" alt="" />
+            <h1 className="text-center  text-7xl font-semibold"><span className="hero-text">I'm Showrov Ghosh,</span> frontend developer based in Bangladesh</h1>
             <p>I am a frontend developer from California, USA with 10 years of experience in multiple companies like Microsoft, Tesla and Apple.</p>
             {/* buttons  */}
             <div>
